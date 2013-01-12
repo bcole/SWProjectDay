bc82a1899a5343f64ad19c3fc0f1c4f1ee10a298
Salvador <sea2693@rit.edu>
2013-01-12 18:30:43 -0500

    Made the constructor for Manager and added the office as a parameter. Added the await methd for a barrier to the Manager.

--- SoftwareProjectDay/src/edu/se/se441/Office.java
+++ SoftwareProjectDay/src/edu/se/se441/Office.java
@@ -1,7 +1,11 @@
 // Test
 package edu.se.se441;
 
+import java.util.concurrent.CyclicBarrier;
+
 public class Office {
+	
+	private CyclicBarrier standupMeeting = new CyclicBarrier(4);
 	
 	private boolean confRoom;
 
--- SoftwareProjectDay/src/edu/se/se441/threads/Manager.java
+++ SoftwareProjectDay/src/edu/se/se441/threads/Manager.java
@@ -1,5 +1,30 @@
 package edu.se.se441.threads;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+import edu.se.se441.Office;
+
 public class Manager extends Thread {
+	private Office office;
+	private CyclicBarrier standupMeeting;
+	
+	public Manager(Office office){
+		this.office = office;
+	}
+	
+	public void run(){
+		try {
+			standupMeeting.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setStandupBarrier(CyclicBarrier standupBarrier){
+		standupMeeting = standupBarrier;
+	}
 
 }
