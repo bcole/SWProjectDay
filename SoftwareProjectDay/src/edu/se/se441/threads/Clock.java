cfd535b5b87a58a5efb23885c3605d668426dbd0
Brandon Cole <bnc8088@rit.edu>
2013-01-12 20:24:17 -0500

    Created run() in Clock

--- SoftwareProjectDay/src/edu/se/se441/threads/Clock.java
+++ SoftwareProjectDay/src/edu/se/se441/threads/Clock.java
@@ -5,9 +5,22 @@ import java.util.concurrent.CountDownLatch;
 public class Clock extends Thread{
 	
 	private CountDownLatch startSignal;
+	private long startTime;	// When the simulation starts.
 	
 	public Clock(CountDownLatch startSignal){
 		this.startSignal = startSignal;
+	}
+	
+	public void run(){
+		try {
+			// Starting all threads at the same time (clock == 0 / "8:00AM").
+			startSignal.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// Set the start time of the simulation.
+		startTime = System.currentTimeMillis();
 	}
 
 }
