cecf28a8b174fb27511d58fcab64e896904c7464
Salvador <sea2693@rit.edu>
2013-01-15 23:53:54 -0500

    Fixed manager bugs

--- SoftwareProjectDay/src/edu/se/se441/threads/Manager.java
+++ SoftwareProjectDay/src/edu/se/se441/threads/Manager.java
@@ -37,7 +37,7 @@ public class Manager extends Thread {
 			if(office.getTime() == 800){
 				System.out.println(office.getStringTime() + " Manager arrives at office");
 			} else {
-				System.out.println("800 Manager arrives at office");
+				System.out.println("8:00 Manager arrives at office");
 			}
 			// Waiting for team leads for the meeting.
 			office.waitForStandupMeeting();
@@ -49,54 +49,61 @@ public class Manager extends Thread {
 		while(office.getTime() < 1700){
 //			System.out.println(office.getStringTime() + " Starting while loop.");
 			office.startWorking();
+			
 			while(!hasQuestion.isEmpty()){
 				answerQuestion();
+				checkConditions();
 			}
-			if(office.getTime() >= 1000 && !attendedMeeting1){
-				 try {
-					System.out.println(office.getStringTime() + " Manager goes to meeting");
-					sleep(600);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				 attendedMeeting1 = true;
-			}
-			if(office.getTime() >= 1200 && !ateLunch){
-				try {
-					System.out.println(office.getStringTime() + " Manager goes to lunch");
-					sleep(600);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}				
-				ateLunch = true;
-			}
-			if(office.getTime() >= 1400 && !attendedMeeting2){
-				try {
-					System.out.println(office.getStringTime() + " Manager goes to meeting");
-					sleep(600);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				attendedMeeting2 = true;
-			}
+			checkConditions();
 			
-			// Is it time for the 4 oclock meeting?
-			if(office.getTime() >= 1600 && !attendedFinalMeeting){
-				office.waitForEndOfDayMeeting();
-				try {
-					System.out.println(office.getStringTime() + " Manager heads to end of day meeting");
-					sleep(150);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				attendedFinalMeeting = true;
-			}
 		}
 		System.out.println(office.getStringTime() + " Manager leaves");
 	}
 	
 	public void setStartSignal(CountDownLatch startSignal) {
 		this.startSignal = startSignal;
+	}
+	
+	public void checkConditions(){
+		if(office.getTime() >= 1000 && !attendedMeeting1){
+			 try {
+				System.out.println(office.getStringTime() + " Manager goes to meeting");
+				sleep(600);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			 attendedMeeting1 = true;
+		}
+		if(office.getTime() >= 1200 && !ateLunch){
+			try {
+				System.out.println(office.getStringTime() + " Manager goes to lunch");
+				sleep(600);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}				
+			ateLunch = true;
+		}
+		if(office.getTime() >= 1400 && !attendedMeeting2){
+			try {
+				System.out.println(office.getStringTime() + " Manager goes to meeting");
+				sleep(600);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			attendedMeeting2 = true;
+		}
+		
+		// Is it time for the 4 oclock meeting?
+		if(office.getTime() >= 1600 && !attendedFinalMeeting){
+			office.waitForEndOfDayMeeting();
+			try {
+				System.out.println(office.getStringTime() + " Manager heads to end of day meeting");
+				sleep(150);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			attendedFinalMeeting = true;
+		}
 	}
 	
 	public void askQuestion(Employee employee){
@@ -147,22 +154,30 @@ public class Manager extends Thread {
 	
 	private void answerQuestion(){
 		Employee employee = hasQuestion.poll();
-		synchronized(questionLock){
-			questionLock.notifyAll();
-		}
-		
-		try {
-			System.out.println(office.getStringTime() + " Manager starts answering question.  Queue depth: " + hasQuestion.size());
-			sleep(100);
-			System.out.println(office.getStringTime() + " Manager ends answering question.  Queue depth: " + hasQuestion.size());
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		employee.questionAnswered();
-		
-		synchronized(questionLock){
-			questionLock.notifyAll();
+		if(office.getTime() < 1700){
+			synchronized(questionLock){
+				questionLock.notifyAll();
+			}
+			
+			try {
+				System.out.println(office.getStringTime() + " Manager starts answering question.  Queue depth: " + hasQuestion.size());
+				sleep(100);
+				System.out.println(office.getStringTime() + " Manager ends answering question.  Queue depth: " + hasQuestion.size());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			employee.questionAnswered();
+			synchronized(questionLock){
+				questionLock.notifyAll();
+			}
+		} else{
+			while(!hasQuestion.isEmpty()){
+				hasQuestion.poll();
+			}
+			synchronized(questionLock){
+				questionLock.notifyAll();
+			}
 		}
 	}
 
