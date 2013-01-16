cfdaa4575a2b1cc11f2444373b347172d4b32066
mhickson <mhickson@129.21.40.189>
2013-01-15 20:11:01 -0500

    added random and call notify working on office

--- SoftwareProjectDay/src/edu/se/se441/threads/Clock.java
+++ SoftwareProjectDay/src/edu/se/se441/threads/Clock.java
@@ -1,13 +1,17 @@
 package edu.se.se441.threads;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
+
+import edu.se.se441.Office;
 
 public class Clock extends Thread{
 	
 	private CountDownLatch startSignal;
 	private long startTime;	// When the simulation starts.
 	private ArrayList<Long> timeRegistry;
+	private Office office;
 	
 	public Clock(CountDownLatch startSignal){
 		this.startSignal = startSignal;
@@ -21,14 +25,21 @@ public class Clock extends Thread{
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+		Random r = new Random();
 		// Set the start time of the simulation.
 		startTime = System.currentTimeMillis();
 		while(this.getTime() <= 5400){ //Simulation starts at 800 (time 0000) and ends at 1700 (time 5400).
 			for(Long t : timeRegistry){
+				int random = r.nextInt(5);
 				if(this.getTime() >= t){
-					notifyAll();
+					o.notifyWorking();
 				}
+				else{
+					if(random == 0){
+						o.notifyWorking();
+					}
+				}
+				
 			}
 		}
 	}
